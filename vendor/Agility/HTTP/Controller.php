17da55a20df2b40342fd7d4a5efad2a13e196a96
HimanshuM <himanshu.malpande@gmail.com>
2017-12-21 16:16:03 +0530

    Added jsPath & cssPath to controller

--- vendor/Agility/HTTP/Controller.php
+++ vendor/Agility/HTTP/Controller.php
@@ -19,6 +19,8 @@ use Agility\HTTP\Mime\MimeTypes;
 		protected $sessionAutoInit;
 
 		protected $viewPath;
+		protected $jsPath;
+		protected $cssPath;
 
 		private $_beforeSubscribers = [];
 		private $_afterSubscribers = [];
@@ -45,6 +47,8 @@ use Agility\HTTP\Mime\MimeTypes;
 			}
 
 			$this->viewPath = (Application::getApplicationInstance())->getPath("viewsDir");
+			$this->jsPath = (Application::getApplicationInstance())->getPath("jsDir");
+			$this->cssPath = (Application::getApplicationInstance())->getPath("cssDir");
 
 			$this->session = new Session;
 
@@ -100,7 +104,7 @@ use Agility\HTTP\Mime\MimeTypes;
 
 			$this->_actionRendered = true;
 
-			$view = new Render\View($this->viewPath);
+			$view = new Render\View($this->viewPath, $this->jsPath, $this->cssPath);
 
 			if ($layout === true) {
 				$view->setLayout("shared/base");
